adef17e210996f7b8bc000522cf680aeb17260d2
Qirion <gabriel.mfcorreia@gmail.com>
2013-05-16 08:54:56 -0300

    Update exemplofuncao.java
    exemplo de função

--- exemplofuncao.java
+++ exemplofuncao.java
@@ -1,3 +1,16 @@
 class programa{
-
+    public static void main(String args[]){
+        double paramero1,parametro2;
+        funcao(parametro1,parametro2);
+    }
+// Public -> pode ser uzada por todos;
+// Static -> não precisa ser objeto
+// double -> Tipo de retorno (retorna um valor Double no caso);
+// funcao(double a,double b) ->passa o nome da função e os parametros de entrada
+// return -> é o valor a ser retrnado
+    public static double funcao(double a,double b){
+        double resultado;
+        resultado = a+b;
+        return resultado;
+    }
 }
